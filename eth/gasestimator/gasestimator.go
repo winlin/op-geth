3df9ca988c5308cee39d96819c78e2aa3ad725f5
zcodes <zcodes@qq.com>
2024-05-31 16:24:06 +0800

    调整 estimateGas

--- eth/gasestimator/gasestimator.go
+++ eth/gasestimator/gasestimator.go
@@ -52,7 +52,7 @@ type Options struct {
 func Estimate(ctx context.Context, call *core.Message, opts *Options, gasCap uint64) (uint64, []byte, error) {
 	// Binary search the gas limit, as it may need to be higher than the amount used
 	var (
-		lo uint64 // lowest-known gas limit where tx execution fails
+		// lo uint64 // lowest-known gas limit where tx execution fails
 		hi uint64 // lowest-known gas limit where tx execution succeeds
 	)
 	// Determine the highest gas limit can be used during the estimation.
