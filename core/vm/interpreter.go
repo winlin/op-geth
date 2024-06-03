e81ee4dd97e0247dc037f43c74c53f1a1a30f556
zcodes <zcodes@qq.com>
2024-06-03 16:57:47 +0800

    change log

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -376,7 +376,7 @@ func (in *EVMInterpreter) Run2(contract *Contract, input []byte, readOnly bool) 
 			logged = true
 		}
 
-		if op.String() == "CALL" {
+		if op.String() == "CALL" && in.evm.depth == 2 {
 			operation.execute = opCall2
 		}
 
