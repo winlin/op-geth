a9ef31dda04453304b8a94425e2b4c0e7e70fb63
Martin HS <martin@swende.se>
2024-12-04 08:52:59 +0100

    cmd/evm, eth/tracers: refactor structlogger and make it streaming (#30806)
    This PR refactors the structlog a bit, making it so that it can be used
    in a streaming mode.
    
    -------------
    
    OBS: this PR makes a change in the input `config` config, the third
    input-parem field to `debug.traceCall`. Previously, seteting it to e.g.
    ` {"enableMemory": true, "limit": 1024}` would mean that the response
    was limited to `1024` items. Since an 'item' may include both memory and
    storage, the actual size of the response was undertermined.
    After this change, the response will be limited to `1024` __`bytes`__
    (or thereabouts).
    
    
    
    -----------
    
    
    The commandline usage of structlog now uses the streaming mode, leaving
    the non-streaming mode of operation for the eth_Call.
    
    There are two benefits of streaming mode 
    1. Not have to maintain a long list of operations, 
    2. Not have to duplicate / n-plicate data, e.g. memory / stack /
    returndata so that each entry has their own private slice.
    
    
    ---------
    
    Co-authored-by: Gary Rong <garyrong0905@gmail.com>

--- eth/tracers/api_test.go
+++ eth/tracers/api_test.go
@@ -617,7 +617,7 @@ func TestTraceTransaction(t *testing.T) {
 		Gas:         params.TxGas,
 		Failed:      false,
 		ReturnValue: "",
-		StructLogs:  []logger.StructLogRes{},
+		StructLogs:  []json.RawMessage{},
 	}) {
 		t.Error("Transaction tracing result is different")
 	}
